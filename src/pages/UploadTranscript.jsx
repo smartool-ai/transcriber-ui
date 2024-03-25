d5cce86121e62c086262d48d91096938ccc31989
Jpifer13 <jakepifer3@gmail.com>
2024-03-24 20:46:09 -0700

    get setFileContent from context

--- src/pages/UploadTranscript.jsx
+++ src/pages/UploadTranscript.jsx
@@ -25,6 +25,7 @@ export default function UploadTranscript() {
 		uploadResponse,
 		setUploadResponse,
 		fileContent,
+		setFileContent,
 	} = useContext(UploadTranscriptContext);
 	const apiRequest = useRequest();
 

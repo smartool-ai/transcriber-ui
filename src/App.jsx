3494fd874f0beacc1987017bfbbb61c9dcde6f44
jimmy-e <jimmy.e@outlook.com>
2024-03-25 14:54:00 -0500

    updating structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { jwtDecode } from 'jwt-decode';
 import { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Router, Route } from "wouter";
-import useHashLocation from '../hooks/useHashLocation';
+import useHashLocation from './hooks/useHashLocation';
 import Spinner from './Spinner';
-import Layout from './Layout';
-import UploadTranscript from '../views/UploadTranscript';
-import DeleteUser from '../views/DeleteUser';
-import WelcomePage from './WelcomePage';
-import HomePage from './HomePage';
-import AddPlatformKeys from '../views/AddPlatformKeys';
-import UserSettings from '../views/UserSettings';
+import Layout from './components/layout/Layout.jsx';
+import UploadTranscript from './views/UploadTranscript';
+import DeleteUser from './views/DeleteUser';
+import WelcomePage from './views/WelcomePage';
+import HomePage from './views/HomePage';
+import AddPlatformKeys from './views/AddPlatformKeys';
+import UserSettings from './views/UserSettings';
 
 export default function App() {
   const {
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react';
 import { UploadTranscriptContextProvider } from './context/UploadTranscriptContext';
-import App from './components/App'
+import App from './App'
 import './index.css'
 
 const appRoot = (window.location.origin + window.location.pathname).replace(/\/$/, '');

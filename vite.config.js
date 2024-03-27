36a164eb3b4bd91017df376563671a43ec2d145d
Jpifer13 <jakepifer3@gmail.com>
2024-03-27 13:20:22 -0700

    Update API URL and environment variables

--- vite.config.js
+++ vite.config.js
@@ -8,21 +8,21 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     base: '',
-    define: mode === 'development' ? {
+    define: {
       "__AUTH0_DOMAIN__": JSON.stringify(env.AUTH0_DOMAIN),
       "__AUTH0_CLIENT_ID__": JSON.stringify(env.AUTH0_CLIENT_ID),
       "__AUTH0_AUDIENCE__": JSON.stringify(env.AUTH0_AUDIENCE),
       "__REACT_APP_STRIPE_CLIENT_SECRET__": JSON.stringify(env.REACT_APP_STRIPE_CLIENT_SECRET),
       "__REACT_APP_STRIPE_PUBLIC_KEY__": JSON.stringify(env.REACT_APP_STRIPE_PUBLIC_KEY),
       "__API_URL__": JSON.stringify(env.API_URL),
-    } : {},
+    },
     build: {
       outDir: './dist',
       emptyOutDir: true,
     },
     server: {
       proxy: {
-        '/api': 'http://127.0.0.1:8000'
+        '/api': JSON.stringify(env.API_URL),
       },
     },
   }

e0875c084a1df346bf701ce27398de8169b4bca2
jimmy-e <jimmy.e@outlook.com>
2024-03-28 11:45:05 -0500

    updating app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,6 +4,8 @@ import Layout from '../components/layout/Layout.jsx';
 import Routes from "./Routes.jsx";
 
 const App = () => {
+  const [location] = useHashLocation();
+
   return (
     <Router hook={useHashLocation}>
       <Layout current={location}>

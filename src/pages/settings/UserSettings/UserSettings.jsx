96e247ffa44abbb137df803a93c153f16be239af
jimmy-e <jimmy.e@outlook.com>
2024-03-29 08:53:38 -0500

    updating user settings

--- src/pages/settings/UserSettings/UserSettings.jsx
+++ src/pages/settings/UserSettings/UserSettings.jsx
@@ -34,66 +34,33 @@ const UserSettings = () => {
 
     return (
       <SettingsLayout>
-          <div className="flex flex-col gap-y-3">
-              <label htmlFor="email" className="label">
-                  Jira Email
-              </label>
-              <input
-                id="email"
-                type="email"
-                // value={email} onChange={e => handleEmailChange(e.target.value)}
-                className="input"
-                placeholder="you@example.com"
-              />
-              <label htmlFor="server" className="label">
-                  Jira Server Adress
-              </label>
-              <input
-                id="server"
-                type="server"
-                // value={server} onChange={e => handleServerChange(e.target.value)}
-                className="input"
-                placeholder="https://yourcompany.atlassian.net"
-              />
-              <label htmlFor="apiKey" className="label">
-                  Jira API Key
-              </label>
-              <input
-                id="apiKey"
-                type="apiKey"
-                // value={apiKey} onChange={e => handleApiKeyChange(e.target.value)}
-                className="input"
-                placeholder="your-api-key"
-              />
-          </div>
-          <div className="flex flex-col space-y-8">
-              <form className="flex flex-col space-y-10" onSubmit={handleSubmit}>
-                  <div className="flex space-x-4 items-center">
-                      <label className="label">Name:</label>
-                      <input
-                        className="input"
-                        onChange={handleNameChange}
-                        type="text"
-                        value={name}
-                      />
-                  </div>
-                  <div>
-                      <button
-                        className={classNames(
-                          submitButtonDisabled ? 'btn-disabled' : '',
-                          "btn"
-                        )}
-                        disabled={submitButtonDisabled}
-                        type="submit"
-                      >
-                          Save
-                      </button>
-                  </div>
-              </form>
-              <div className="flex flex-col space-y-4">
-                  <a className="link">Reset Password</a>
+          <form className="flex flex-col space-y-10" onSubmit={handleSubmit}>
+              <div className="flex flex-col gap-y-3">
+                  <label htmlFor="name" className="label">
+                      Name
+                  </label>
+                  <input
+                    className="input"
+                    onChange={handleNameChange}
+                    type="text"
+                    value={name}
+                    id="name"
+                  />
+                  <a className="my-4 link">Reset Password</a>
               </div>
-          </div>
+              <div>
+                  <button
+                    className={classNames(
+                      submitButtonDisabled ? 'btn-disabled' : '',
+                      "btn"
+                    )}
+                    disabled={submitButtonDisabled}
+                    type="submit"
+                  >
+                      Save
+                  </button>
+              </div>
+          </form>
       </SettingsLayout>
     );
 };

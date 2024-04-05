6c50ba00e31a508fcd2992b67e2bb56b539bdbef
Renerose Dimatulac <rsdimatulac@gmail.com>
2024-04-04 22:16:06 -0700

    Remove 'relative' to fix toast popping under generate button

--- src/components/tables/UploadedFilesTable.jsx
+++ src/components/tables/UploadedFilesTable.jsx
@@ -4,7 +4,7 @@ export default function UploadedFilesTable({ generateTickets, files, ticketsResp
 	const generateTicketsButton = (name) => (
 		<td className="py-3">
 			<button
-				className="relative cursor-pointer flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#4654A3] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+				className="cursor-pointer flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#4654A3] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
 				onClick={() => generateTickets(name)}
 			>
 				{isPolling ? (

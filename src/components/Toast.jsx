e222f6da3f2947d8a7f5f1681c7053e813f2ca9b
Renerose Dimatulac <rsdimatulac@gmail.com>
2024-04-04 21:53:34 -0700

    Make toast sticky to the window regardless of scroll

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -26,26 +26,28 @@ export default function Toast({ label, onClose, type = "info" }) {
 
 	return (
 		<div className="relative">
-			<Transition
-				appear
-				as={Fragment}
-				enter="transform ease-out duration-300 transition"
-				enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-				enterTo="translate-y-0 opacity-100 sm:translate-x-0"
-				show
-			>
-				<div className={["rounded-md p-4 flex gap-2 max-w-fit absolute z-10 right-0", bgColor[type]].join(" ")}>
-					<div className="h-5 w-5 text-white" aria-hidden="true">
-						{alertType[type]}
+			<div className="fixed h-14 inset-x-5 inset-y-20 lg:inset-10">
+				<Transition
+					appear
+					as={Fragment}
+					enter="transform ease-out duration-300 transition"
+					enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+					enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+					show
+				>
+					<div className={["rounded-md p-4 flex gap-2 max-w-fit absolute right-0", bgColor[type]].join(" ")}>
+						<div className="h-5 w-5 text-white" aria-hidden="true">
+							{alertType[type]}
+						</div>
+						<p className="text-sm text-white font-semibold">
+							{label}
+						</p>
+						<div className="h-5 w-5 text-white font-bold hover:border-2 hover:rounded-md ml-1" onClick={onClose}>
+							<XMarkIcon />
+						</div>
 					</div>
-					<p className="text-sm text-white font-semibold">
-						{label}
-					</p>
-					<div className="h-5 w-5 text-white font-bold hover:border-2 hover:rounded-md ml-1" onClick={onClose}>
-						<XMarkIcon />
-					</div>
-				</div>
-			</Transition>
+				</Transition>
+			</div>
 		</div>
 	)
 }
